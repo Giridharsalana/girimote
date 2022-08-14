98a86f60ba3b0433c071ffa9c5d8217c6dc67ea1
Giridhar Salana <37552476+Giridharsalana@users.noreply.github.com>
2022-08-14 12:59:24 +0000

    Updates

--- android/app/src/main/kotlin/com/example/girimote/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/girimote/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.gemote
+package com.example.girimote
 
 import io.flutter.embedding.android.FlutterActivity
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:gemote/main.dart';
+import 'package:girimote/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
